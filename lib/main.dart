e9240cd6730e8bc6c86b1f8598954696381933c8
frost127 <59542863+eshaan127@users.noreply.github.com>
2023-11-28 20:57:45 -0800

    settings page framework

--- lib/main.dart
+++ lib/main.dart
@@ -81,3 +81,19 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: const Center(
+        child: Text('This is the Settings Page'),
+      ),
+    );
+  }
+}
